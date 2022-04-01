68f9e7f4eff89dcb7759be69095348840b7cf3de
xasanboy <xasanboyorifjonov2@gmail.com>
2022-04-01 15:50:59 +0500

    end project

---
+++ main.js
@@ -0,0 +1,42 @@
+const btn1=document.querySelector(".btn_1");
+const btn2=document.querySelector(".btn_2");
+const btn3=document.querySelector(".btn_3");
+const btn4=document.querySelector(".btn_4");
+const btn5=document.querySelector(".btn_5");
+const btn6=document.querySelector(".btn_6");
+const btn7=document.querySelector(".btn_7");
+const btn8=document.querySelector(".btn_8");
+
+btn1.addEventListener("click", bgc1);
+function bgc1(){
+   document.body.style.background="black"
+}
+btn2.addEventListener("click", bgc2);
+function bgc2(){
+   document.body.style.background="#E42597"
+}
+btn3.addEventListener("click", bgc3);
+function bgc3(){
+   document.body.style.background="#BD3DD2"
+}
+btn4.addEventListener("click", bgc4);
+function bgc4(){
+   document.body.style.background="#F11F1E"
+}
+btn5.addEventListener("click", bgc5);
+function bgc5(){
+   document.body.style.background="#56DB41"
+}
+btn6.addEventListener("click", bgc6);
+function bgc6(){
+   document.body.style.background="#4000F4"
+}
+btn7.addEventListener("click", bgc7);
+function bgc7(){
+   document.body.style.background="#EFA9A9"
+}
+btn8.addEventListener("click", bgc8);
+function bgc8(){
+   document.body.style.background="#C20677"
+}
+
